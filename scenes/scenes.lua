e6e9084d0099a861a4c4d34e7e27651c26a29f87
d954mas <d954mas@mail.ru>
2017-04-03 20:26:53 +0300

    load default_scene at start

--- scenes/scenes.lua
+++ scenes/scenes.lua
@@ -6,7 +6,10 @@ local function create_url(name)
 	return msg.url(prefix .. name)
 end	
 
+
 M.scene1 = create_url("#scene1")
 M.scene2 = create_url("#scene2")
+
+M.default_scene_name = "scene1"
 
 return M
